f98f90063c24cdff79ca25ff32e8cd2b2e0a47aa
alessio presciuttini <presciuttinia@gmail.com>
2023-03-14 16:19:45 +0100

    Appunto

--- Alessio.Exercist/Program.cs
+++ Alessio.Exercist/Program.cs
@@ -11,31 +11,40 @@ namespace Alessio.Exercist
             Continente continente = new Continente(nome);
 
             //Agregazione
-            string nome_regione     = "REGIONE";
-            string nome_provincia   = "PROVINCIA";
-            string nome_comunune    = "COMUNE";
-            string nome_abitante    = "ABITANTE";
+            string nome_regione = "REGIONE";
+            string nome_provincia = "PROVINCIA";
+            string nome_comunune = "COMUNE";
+            string nome_abitante = "ABITANTE";
 
 
             //
-            Regione regione         = new Regione(continente._paese, nome_regione);
+            Regione regione = new Regione(continente._paese, nome_regione);
             //continente._paese.AddRegione(regione);
-            Provincia provincia     = new Provincia(regione, nome_provincia);
+            Provincia provincia = new Provincia(regione, nome_provincia);
             //regione.AddProvincia(provincia);
-            Comune comune           = new Comune(provincia, nome_comunune);
+            Comune comune = new Comune(provincia, nome_comunune);
             //provincia.AddComune(comune);
-            Abitanti abitante       = new Abitanti(comune, nome_abitante);
+            Abitanti abitante = new Abitanti(comune, nome_abitante);
             //comune.AddCittadino(abitante);
-            Unione_europea europa   = new Unione_europea(continente._paese);
+            Unione_europea europa = new Unione_europea(continente._paese);
 
             continente._paese.AddOrganizationUE(europa);
-            //europa.AggiungiStato(continente._paese);
-            ONU onu                 = new ONU(continente._paese);
+            ONU onu = new ONU(continente._paese);
             continente._paese.AddOrganizationONU(onu);
-           //onu.AggiungiStato(continente._paese);
-            NATO nato               = new NATO(continente._paese);
+            NATO nato = new NATO(continente._paese);
             continente._paese.AddOrganizationNATO(nato);
+
+            /////
+            ///
+            //Non credo che la relazione di Aggregazione tra paese e le organizzazione sia corretta,
+            //credo che la relazione giusta sia di dipendenza tra le organizzazioni e il paese
+            //quindi ho implementato una versione alternativa 
+            //Unione_europea europa = new Unione_europea(continente._paese);
+            //europa.AggiungiStato(continente._paese);
+            //ONU onu = new ONU(continente._paese);
             //onu.AggiungiStato(continente._paese);
+            //NATO nato = new NATO(continente._paese);
+            //nato.AggiungiStato(continente._paese);
         }
     }
 }
