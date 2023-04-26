59f77d042fc3ac0261e44b7e811ec7e1bc6ba20d
alessio presciuttini <presciuttinialessio@gmail.com>
2023-04-26 15:22:20 +0200

    ex 6 e 7

---
+++ Alessio.Exersise6/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Alessio.Exersises6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dir = "E:\\";
+            string filename = "filename.csv";
+            string filePath = Path.Combine(dir, filename);
+
+            List<Person> people = new List<Person>();
+            people.Add(new() { Name = "franco", Surname = "Bruno", CF = "BRNFRC65T44F4443" });
+            people.Add(new() { Name = "LUCA", Surname = "Bruno", CF = "BRNFRC65T44F4443" });
+            people.Add(new() { Name = "MARCO", Surname = "Bruno", CF = "BRNFRC65T44F4443" });
+            people.Add(new() { Name = "luisa", Surname = "Bruno", CF = "BRNFRC65T44F4443" });
+           
+            TextFileGenerator.SaveToFile(people, filePath);
+            var newList = TextFileGenerator.LoadFromCSVFile(people[0], filePath);
+
+            if (newList != null)
+            {
+                foreach (var item in newList)
+                {
+                    Console.WriteLine($"{item.Name}, {item.Surname}, {item.CF}");
+                }
+            }
+        }
+
+        public static class TextFileGenerator
+        {
+
+            //Serialization 
+            public static void SaveToFile<T>(List<T> data, string filePath)
+            {
+                StringBuilder line = new StringBuilder();
+
+                //check
+                if (data == null || data.Count == 0) return;
+
+                //retrieve properties
+                var cols = data[0].GetType().GetProperties();
+
+                //file not exists create header
+                if (!File.Exists(filePath))
+                {
+                    foreach (var col in cols)
+                    {
+                        string propName = col.Name;
+                        line.Append(propName);
+
+                        if (col != cols.Last())
+                        {
+                            line.Append(',');
+                        }
+                        else
+                        {
+                            line.Append('\n');
+                        }
+                    }
+
+                    File.AppendAllText(filePath, line.ToString());
+                }
+
+                //create rows 
+                foreach (var row in data)
+                {
+                    line = new StringBuilder();
+
+                    foreach (var col in cols)
+                    {
+                        var value = col.GetValue(row);
+                        line.Append(value);
+
+                        if (col != cols.Last())
+                        {
+                            line.Append(',');
+                        }
+                        else
+                        {
+                            line.Append('\n');
+                        }
+                    }
+
+                    File.AppendAllText(filePath, line.ToString());
+                }
+            }
+
+            //Deserialization 
+            public static List<T> LoadFromCSVFile<T>(T data, string filePath) where T : class, new()
+            {
+                string[] rows = File.ReadAllLines(filePath);
+                var cols = data.GetType().GetProperties();
+
+                //check if header file is contained in T props
+                List<string> propsNameList = new List<string>();
+                string[] headerWordsAray = rows[0].Split(',');
+
+                foreach (var col in cols)
+                {
+                    propsNameList.Add(col.Name);
+                }
+
+                foreach (var headerWord in headerWordsAray)
+                {
+                    if (!propsNameList.Contains(headerWord)) return null;
+                }
+
+                //new list with T objects
+                List<T> list = new List<T>();
+
+                foreach (var row in rows)
+                {
+                    if (row == rows.First()) continue;
+
+                    T obj = new();
+                    string[] rowWords = new string[cols.Length];
+
+                    if (row != rows.First())
+                    {
+                        rowWords = row.Split(',');
+                    }
+
+                    for (int i = 0; i < headerWordsAray.Length; i++)
+                    {
+                        //maybe rowWords and cols should be sorted to avoid bugs
+                        var convertedValue = Convert.ChangeType(rowWords[i], cols[i].PropertyType);
+                        obj.GetType().GetProperty(cols[i].Name).SetValue(obj, convertedValue);
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string CF { get; set; }
+        }
+    }
+}
---
+++ Alessio.Exersise7/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+
+namespace Alessio.Exersises7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Dato un certo elemento ordinare la lista in ordine crescente decrescente
+            ///
+
+            Person alessio = new Person { Name = "Alessio", Surname = "Presciuttini", CF = "PRSLSS92L10F499Q" };
+            Person matteo = new Person { Name = "Matteo", Surname = "Luccisano", CF = "LCSMTT98T15G111T" };
+            Person cristina = new Person { Name = "Alex", Surname = "sircu", CF = "CRCALX97G21G111K" };
+            Person fausto = new Person { Name = "Fausto", Surname = "Boshoff", CF = "BSFFST92G11H501W" };
+            Person Mario = new Person { Name = "Mario", Surname = "Di Stefano", CF = "DSTMNC97" };
+            List<Person> people = new List<Person>
+            {
+                alessio, matteo, cristina, fausto
+            };
+            
+            people = Ordered.OrderListAscended(people,"Name");
+
+
+            people = Ordered.OrderListDiscendent(people,"Name");
+
+            foreach (Person person in people)
+                Console.WriteLine(person.Name);
+
+            people = Ordered.OrdinaPerProprieta(people, p => p.Surname);
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine($"il nome " + people[i].Name + " " + people[i].Surname);
+
+            }
+            List<int> listint = new List<int>() { 1, 2, 3 };
+            listint = Ordered.OrdinaPerProprieta(listint, l => l, false);
+            for (int i = 0; i < listint.Count; i++)
+            {
+                Console.WriteLine(listint[i]);
+
+            }
+        }
+    }
+
+    public class Ordered
+    {
+    public  static List<T> OrderListAscended<T>(List<T> data ,string typeobj)
+        {
+            var asclist = data.OrderBy(x => x.GetType().GetProperty(typeobj).GetValue(x)).ToList();
+            return asclist;
+        }
+        public static List<T> OrderListAscended2<T>(List<T> data ,string typeobj)
+        {
+            var datalist = data.GetType().GetField(typeobj).GetValue(data);
+
+            for (int i = 0; i < data.Count; i++)
+            {
+
+            }
+            return asclist;
+        }
+      
+        public static List<T> OrderListDiscendent<T>(List<T> data,string name)
+        {
+         var deslist=   data.OrderByDescending(x => x.GetType().GetProperty(name).GetValue(x)).ToList();
+            return deslist;
+        }
+
+        public void OrderListByElement<T>(T data)
+    {
+
+    }
+        public static List<T> OrdinaPerProprieta<T, TKey>(List<T> lista, Func<T, TKey> selezioneProprieta, bool ordineCrescente = true)
+        {
+            if (ordineCrescente)
+            {
+                return lista.OrderBy(selezioneProprieta).ToList();
+            }
+            else
+            {
+                return lista.OrderByDescending(selezioneProprieta).ToList();
+            }
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CF { get; set; }
+    }
+}
